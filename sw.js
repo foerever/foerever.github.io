eb716da3e38079d64ea63339ae023221c8bd9ed6
Sung Soo (Anthony) Cho <scho379@gmail.com>
2022-01-17 23:20:22 -0800

    Updates

--- sw.js
+++ sw.js
@@ -27,10 +27,10 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-a2781e1ea8570e9fc3a3.js"
+    "url": "webpack-runtime-b75f46d5247a49cbc491.js"
   },
   {
-    "url": "styles.e1a70a9d67ced3018652.css"
+    "url": "styles.2d74e9926a71b30b2054.css"
   },
   {
     "url": "styles-9b6f388623a2ec93d35f.js"
@@ -46,7 +46,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "01120454f089752b712d4f69d80388b2"
+    "revision": "9b6f80f82734a69497d27d30adbda76d"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-859e22a7cba0ed2e01d8.js"
